f866aa40d5a3b63df2f7453002f4312e952af4ee
Renato <rverissimo.dev@gmail.com>
2025-06-27 13:57:44 -0300

    feat: add basic config for psql database

--- app/database.py
+++ app/database.py
@@ -1,3 +1,21 @@
 import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql+asyncpg://orderuser:orderpassword@order_db:5432/order_db')
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def init_db():
+    pass
+
+async def get_db():
+    pass
