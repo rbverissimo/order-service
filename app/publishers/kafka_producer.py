dd619a115422e50d9f4f88f57e5426f04092698f
Renato <rverissimo.dev@gmail.com>
2025-07-30 11:52:48 -0300

    feat: add logic into senting event method

--- app/publishers/kafka_producer.py
+++ app/publishers/kafka_producer.py
@@ -1,9 +1,12 @@
 from aiokafka import AIOKafkaProducer
-from ..core.config import Settings
+from app.core.config import Settings
 from app.schemas import OrderCreatedEvent
 
 
 class KafkaProducerService:
+
+    TOPIC_ORDER_CREATED = 'order-created'
+
     def __init__(self, bootstrap_servers: str):
         self.producer = AIOKafkaProducer(bootstrap_servers=bootstrap_servers)
 
@@ -16,7 +19,15 @@ class KafkaProducerService:
         print('Kafka producer stopped!')
     
     async def publish_to_order_created_topic(self, event: OrderCreatedEvent):
-        pass
+        try:
+            topic = self.TOPIC_ORDER_CREATED
+            message_payload = event.model_dump_json().encode('utf-8')
+            key=str(event.orderId).encode('utf-8')
+            await self.producer.send_and_wait(topic, message_payload, key) 
+            print(f'Message sent to topic {topic}: {message_payload}')
+        except Exception as e:
+            print(f'Failed to sent message to topic {topic}: {message_payload}')
+            raise
 
 kafka_producer_service: KafkaProducerService = None
 
